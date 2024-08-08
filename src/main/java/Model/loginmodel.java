2054eae7f080c02838d498168361c2bba3e7d30c
PrajwolKhadka <prazolkhadka67@gmail.com>
2024-08-08 15:33:19 +0545

    Login model added

---
+++ src/main/java/Model/loginmodel.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+
+/**
+ *
+ * @author Acer
+ */
+
+public class loginmodel {
+    private final String email;
+    private final String pass;
+  
+    public  loginmodel(String email, String pass){
+        this.email = email;
+        this. pass= pass;}
+          public String getEmail() {
+        return email;
+    }
+       public String getpassword() {
+        return pass;
+    }
+    }
+
